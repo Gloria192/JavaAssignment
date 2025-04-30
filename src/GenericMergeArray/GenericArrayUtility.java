a040469b4df9bd42329adfa387fc065c0616776e
Gloria <mutuyegloria@gmail.com>
2025-04-30 14:43:45 -0700

    add changes

--- src/GenericMergeArray/GenericArrayUtility.java
+++ src/GenericMergeArray/GenericArrayUtility.java
@@ -6,17 +6,17 @@ public class GenericArrayUtility {
     public static <T> T[] mergeArrays(T[] a, T[] b) {
 
         if (a == null)
-            return b; // Return b if a is null
+            return b;
 
         if (b == null)
-            return a; // Return a if b is null
+            return a;
 
         T[] mergedArray = (T[]) Array.newInstance(a.getClass().getComponentType(), a.length + b.length);
 
         System.arraycopy(a, 0, mergedArray, 0, a.length);
         System.arraycopy(b, 0, mergedArray, a.length, b.length);
 
-        return mergedArray; // Return the merged array
+        return mergedArray; // Return the mergedarray
     }
 }
 
