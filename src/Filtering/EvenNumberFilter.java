4faf331d83d00388aa72396ac646267be99603ac
Gloria <mutuyegloria@gmail.com>
2025-05-01 07:03:41 -0700

    add the second commit

--- src/Filtering/EvenNumberFilter.java
+++ src/Filtering/EvenNumberFilter.java
@@ -1,4 +1,17 @@
 package Filtering;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class EvenNumberFilter {
+    public static void filterEvenNumbers(List<Integer> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            System.out.println("List is empty");
+            return;
+        }
+
+    }
 }
+
+
+
--- src/Filtering/main1.java
+++ src/Filtering/main1.java
@@ -1,4 +1,19 @@
 package Filtering;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static Filtering.EvenNumberFilter.filterEvenNumbers;
+
 public class main1 {
+        public static void main(String[] args) {
+            List<Integer> numbers = Arrays.asList(2, 45, 67, 878, 89, 866, 90, 76, 4);
+            List<Integer> evenNumbers = numbers.stream()
+                    .filter(n -> n % 2 == 0)
+                    .toList();
+            filterEvenNumbers(numbers);
+            System.out.println("the numbers are: " + numbers);
+            System.out.println("the numbers are: " + evenNumbers);
+        }
+
 }
